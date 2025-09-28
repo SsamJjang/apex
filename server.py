b0d8930b62670d5e77255d888ec44a4c2b4f3cc7
ssamjjang <mcwitherian.playz@gmail.com>
2025-09-28 12:09:17 +0900

    server v1

--- server.py
+++ server.py
@@ -2845,4 +2845,4 @@ if __name__ == "__main__":
             db.session.commit()
 
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=True)
+    socketio.run(app, host="0.0.0.0", port=port, debug=False, allow_unsafe_werkzeug=True)
